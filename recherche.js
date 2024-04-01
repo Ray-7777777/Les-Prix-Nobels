60da358d8cdb3dd2beb68a5d1a7deb0109283205
AdrianG1134 <adrianguilhem@yahoo.fr>
2024-04-01 21:11:42 +0200

    Update recherche.js

--- recherche.js
+++ recherche.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
         var items = JSON.parse(storedResults);
         var text = "<ul> ";
         items.forEach(function(element) {
-            text += "<li class='article_recherche' data-id="+element["Id_nominé"]+"><div class='titre_recherche'>"+element["Prénom"]+" "+element["Nom"]+element["Id_nominé"]+"</div><p class='minibio'>"+element["biographie"]+"</p></li>";
+            text += "<li class='article_recherche' data-id="+element["Id_nominé"]+"><div class='titre_recherche'>"+element["Prénom"]+" "+element["Nom"]+"</div><p class='minibio'>"+element["biographie"]+"</p></li>";
         });
         text += "</ul>";
         $(".contenu").html(text);
