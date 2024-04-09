0a0160fd04236001e98f36f8269bfa36dc3d4b35
AdrianG1134 <adrianguilhem@yahoo.fr>
2024-04-09 21:07:17 +0200

    graphiques personnalisés
    2 types de graphiques personnalisés fonctionnels, css à beaucoup compléter et lien avec les autres pages à faire

---
+++ graphiques.php
@@ -0,0 +1,77 @@
+<?php
+    require "bd.php";
+    $bdd = getBD();
+
+    if ($_POST["action"] == "menu") {
+        if ($_POST["id"] == "Gender") {
+            $req = $bdd->prepare("SELECT `Gender` FROM nomine GROUP BY Gender");
+        } 
+        else if ($_POST["id"] == "Année") {
+            $req = $bdd->prepare("SELECT `Année` FROM prix_nobel GROUP BY `Année` ORDER BY `Année` ASC");
+        } 
+        else if ($_POST["id"] == "Category") {
+            $req = $bdd->prepare("SELECT `Nom_catégorie` FROM categorie");
+        } 
+        else if ($_POST["id"] == "Naissance") {
+            $req = $bdd->prepare("SELECT `Born_country` FROM nomine GROUP BY `Born_country`");
+        } 
+        else if ($_POST["id"] == "Décès") {
+            $req = $bdd->prepare("SELECT `Died_country` FROM nomine GROUP BY `Died_country`");
+        } 
+        elseif ($_POST["id"] == "NombrePrix") {
+        }
+
+        $req->execute();
+        $resultat = [];
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $resultat[] = $row;
+        }
+    }
+    elseif ($_POST["action"] == "tracer") {
+        if ($_POST["catX"] == "Gender" || $_POST["catY"] == "Gender") {
+            if ($_POST["catX"] == "Année" || $_POST["catY"] == "Année") {
+                $req = $bdd->prepare("SELECT p.Année, COUNT(*), n.Gender AS Nombre FROM prix_nobel p JOIN nomine n ON p.id_nominé = n.id_nominé GROUP BY p.Année, n.Gender ORDER BY p.Année, n.Gender;");
+            } elseif ($_POST["catX"] == "Category" || $_POST["catY"] == "Category") {
+                $req = $bdd->prepare("SELECT c.Nom_catégorie, COUNT(*), n.Gender AS Nombre FROM prix_nobel p JOIN nomine n ON p.id_nominé = n.id_nominé JOIN categorie c ON p.id_category = c.Id_catégorie GROUP BY c.Nom_catégorie, n.Gender ORDER BY c.Nom_catégorie, n.Gender;");
+            } elseif ($_POST["catX"] == "Naissance" || $_POST["catY"] == "Naissance") {
+                $req = $bdd->prepare("SELECT n.Born_country, COUNT(*), n.Gender AS Nombre FROM nomine n GROUP BY n.Gender, n.Born_country ORDER BY n.Born_country;");
+            } elseif ($_POST["catX"] == "Décès" || $_POST["catY"] == "Décès") {
+                $req = $bdd->prepare("SELECT n.Died_country, COUNT(*), n.Gender AS Nombre FROM nomine n GROUP BY n.Gender, n.Died_country ORDER BY n.Died_country;");
+            } else {
+                $req = $bdd->prepare("SELECT Gender, COUNT(*) AS Nombre FROM nomine GROUP BY Gender;");
+            }
+        } elseif ($_POST["catX"] == "Année" || $_POST["catY"] == "Année") {
+            if ($_POST["catX"] == "Category" || $_POST["catY"] == "Category") {
+                $req = $bdd->prepare("SELECT c.Nom_catégorie, COUNT(*), p.Année AS Nombre FROM prix_nobel p JOIN categorie c ON p.id_category = c.id_catégorie GROUP BY c.Nom_catégorie, p.Année ORDER BY c.Nom_catégorie, p.Année;");
+            } elseif ($_POST["catX"] == "Naissance" || $_POST["catY"] == "Naissance") {
+                $req = $bdd->prepare("SELECT n.Born_country, COUNT(*), p.Année AS Nombre FROM nomine n JOIN prix_nobel p ON n.id_nominé = p.id_nominé GROUP BY n.Born_country, p.Année ORDER BY p.Année, n.Born_country;");
+            } elseif ($_POST["catX"] == "Décès" || $_POST["catY"] == "Décès") {
+                $req = $bdd->prepare("SELECT n.Died_country, COUNT(*), p.Année AS Nombre FROM nomine n JOIN prix_nobel p ON n.id_nominé = p.id_nominé GROUP BY n.Died_country, p.Année ORDER BY p.Année, n.Died_country;");
+            } else {
+                $req = $bdd->prepare("SELECT Année, COUNT(*) AS Nombre FROM prix_nobel GROUP BY Année");
+            }
+        } elseif ($_POST["catX"] == "Category" || $_POST["catY"] == "Category") {
+            if ($_POST["catX"] == "Naissance" || $_POST["catY"] == "Naissance") {
+                $req = $bdd->prepare("SELECT n.Born_country, COUNT(*), c.Nom_catégorie AS Nombre_de_Prix FROM prix_nobel p JOIN nomine n ON p.id_nominé = n.id_nominé JOIN categorie c ON p.id_category = c.id_catégorie GROUP BY n.Born_country, c.Nom_catégorie ORDER BY c.Nom_catégorie, n.Born_country;");
+            } elseif ($_POST["catX"] == "Décès" || $_POST["catY"] == "Décès") {
+                $req = $bdd->prepare("SELECT n.Died_country, COUNT(*), c.Nom_catégorie AS Nombre_de_Prix FROM prix_nobel p JOIN nomine n ON p.id_nominé = n.id_nominé JOIN categorie c ON p.id_category = c.id_catégorie GROUP BY n.Died_country, c.Nom_catégorie ORDER BY c.Nom_catégorie, n.Died_country;");
+            } else {
+                $req = $bdd->prepare("SELECT c.Nom_catégorie, COUNT(p.id_category) AS Nombre_de_Prix FROM categorie c LEFT JOIN prix_nobel p ON c.id_catégorie = p.id_category GROUP BY c.Nom_catégorie;");
+            }
+        } elseif ($_POST["catX"] == "Naissance" || $_POST["catY"] == "Naissance") {
+            if ($_POST["catX"] == "Décès" || $_POST["catY"] == "Décès") {
+                $req = $bdd->prepare("SELECT n1.Born_country AS Pays_de_naissance, COUNT(*) AS Nombre_de_Prix, n2.Born_country AS Pays_de_décès FROM prix_nobel p JOIN nomine n1 ON p.id_nominé = n1.id_nominé JOIN nomine n2 ON p.id_nominé = n2.id_nominé GROUP BY n1.Born_country, n2.Born_country ORDER BY n1.Born_country, n2.Born_country;");
+            } else {
+                $req = $bdd->prepare("SELECT n.Born_country AS Pays_de_naissance, COUNT(*) AS Nombre_de_prix FROM prix_nobel p JOIN nomine n ON p.id_nominé = n.id_nominé GROUP BY n.Born_country ORDER BY n.Born_country;");
+            }
+        } elseif ($_POST["catX"] == "Décès" || $_POST["catY"] == "Décès") {
+            $req = $bdd->prepare("SELECT n.Died_country AS Pays_de_naissance, COUNT(*) AS Nombre_de_prix FROM prix_nobel p JOIN nomine n ON p.id_nominé = n.id_nominé GROUP BY n.Died_country ORDER BY n.Died_country;");
+        }
+        $req->execute();
+        $resultat = [];
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+            $resultat[] = $row;
+        }
+    }
+    echo json_encode($resultat);
+?>
---
+++ graphiques_perso.php
@@ -0,0 +1,119 @@
+<?php
+    session_start();
+    require_once 'connexion_bd.php';
+    $est_connecte = isset($_SESSION['user_id']);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css?v=<?php echo filemtime('style.css'); ?>" type="text/css" media="screen" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	 <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+	 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Lao+Looped:wght@100..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto+Slab:wght@100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+	 <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi+Fun:wght@400..700&display=swap" rel="stylesheet">
+	 
+</head>
+<body>
+    <div id="entete">
+    <a href="index.php" style="text-decoration: none;">
+        <h1 class="oswald-font"><span class="text-stroke" style="color : black;">PRIX NOBEL</span></h1>
+    </a>
+	</div>
+
+<div class="menu" style="width: 100%;">
+    <nav class="navbar navbar-expand-lg navbar-light justify-content-start px-0">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav w-100 justify-content-between">
+                    <li class="nav-item">
+                        <a class="nav-link mx-5" id="accueil" href="index.php" style="color: black;">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-5" id="recherche" href="recherche.php" style="color: black;">Recherche</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mx-5" id="graphique" href="graphique.php" style="color: black;">Graphique</a>
+                    </li>
+                    <li class="nav-item">
+                        <?php
+                        if ($est_connecte) {
+                            echo '<a class="nav-link mx-5" id="connexion" href="connexion.php" style="color: black;">Connecté <span class="status-indicator connected"></span></a>';
+                        } else {
+                            echo '<a class="nav-link mx-5" id="connexion" href="connexion.php" style="color: black;">Connexion <span class="status-indicator disconnected"></span></a>';
+                        }
+                        ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+    <div class="boite">
+        <div class="topBar">
+            <div class="dropdown" id="menuG">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Type de graphe
+               </a>
+                <div class="dropdown-menu">
+                    <button class="dropdown-item typeG" type="button" href="#" id="Barre">Barre</button>
+                    <button class="dropdown-item typeG" type="button" href="#" id="Circulaire">Circulaire</button>
+                    <!-- <button class="dropdown-item typeG" type="button" href="#" id="Linéaire">Linéaire</button>
+                    <button class="dropdown-item typeG" type="button" href="#" id="BarreEmp">Barres empilées</button>
+                    <button class="dropdown-item typeG" type="button" href="#" id="Nuage">Nuage de points</button> -->
+                </div>
+            </div>
+            <div id="varX">
+                <h4 class="titreVariables">Variables sur X:</h4>
+                <!-- Bouton Sexe -->
+                <button type="button" class="btn btn-primary btnVar" data-axe="X" data-nb="menu1" data-cat="Gender" id="GenderX" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sexe</button>
+                <!-- Bouton Année -->
+                <button type="button" class="btn btn-primary btnVar" data-axe="X" data-cat="Année" data-nb="menu2" id="AnnéeX" href="#" role="button" aria-haspopup="true" aria-expanded="false">Année</button>
+                <!-- Categorie -->
+                <button type="button" class="btn btn-primary btnVar" data-axe="X" data-cat="Category" data-nb="menu3" id="CategoryX" href="#" role="button" aria-haspopup="true" aria-expanded="false">Catégorie</button>
+                <!-- Naissance -->
+                <button type="button" class="btn btn-primary btnVar" href="#" data-axe="X" data-cat="Naissance" data-nb="menu4" id="NaissanceX" role="button" aria-haspopup="true" aria-expanded="false">Pays de naissance</button>
+                <!-- Décès -->
+                <button type="button" class="btn btn-primary btnVar" href="#" data-axe="X" data-cat="Décès" data-nb="menu5" id="DécèsX" role="button" aria-haspopup="true" aria-expanded="false">Pays de décès</button>
+                <!-- NombrePrix -->
+                <button type="button" class="btn btn-primary btnVar" href="#" data-axe="X" data-cat="NombrePrix" data-nb="menu6" id="NombrePrixX" role="button" aria-haspopup="true" aria-expanded="false">Nombre de prix Nobel</button>
+            </div>
+            <div id="varY">
+                <h4 class="titreVariables" id="titreY" >Variables sur Y:</h4>
+                <button type="button" class="btn btn-primary btnVar" data-nb="menu7" data-axe="Y" data-cat="Gender" id="GenderY" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sexe</button>
+                <button type="button" class="btn btn-primary btnVar" data-axe="Y" data-nb="menu8" data-cat="Année" id="AnnéeY" href="#" role="button" aria-haspopup="true" aria-expanded="false">Année</button>
+                <button type="button" class="btn btn-primary btnVar" data-axe="Y" data-nb="menu9" data-cat="Category" id="CategoryY" href="#" role="button" aria-haspopup="true" aria-expanded="false">Catégorie</button>
+                <button type="button" class="btn btn-primary btnVar" data-axe="Y" href="#" data-cat="Naissance" data-nb="menu10" id="NaissanceY" role="button" aria-haspopup="true" aria-expanded="false">Pays de naissance</button>
+                <button type="button" class="btn btn-primary btnVar" data-axe="Y" href="#" data-cat="Décès" data-nb="menu11" id="DécèsY" role="button" aria-haspopup="true" aria-expanded="false">Pays de décès</button>
+                <button type="button" class="btn btn-primary btnVar" data-axe="Y" href="#" data-cat="NombrePrix" data-nb="menu12" id="NombrePrixY" role="button" aria-haspopup="true" aria-expanded="false">Nombre de prix Nobel</button>
+            </div>
+            <div id="activeVar">
+                <h6 id="varSelect">Variables selectionnées:</h6>
+                <div class="activeVar" id="activeX">X:<div id="varActX"></div></div>
+                <div class="activeVar" id="activeY">Y:<div id="varActY"></div></div>
+                <div class="activeVar activeGraph" id=""></div>
+            </div>
+        </div>
+        <div class="contenu_graphe">
+            <ul>
+                <div>
+                    <canvas id="graphs" width="400" height="400"></canvas>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="graphiques.js"></script>
+</body>
+</html>
