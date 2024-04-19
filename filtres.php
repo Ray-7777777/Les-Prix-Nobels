dc0e57eef690baccc1051f2db12c2ddfc749c381
AdrianG1134 <adrianguilhem@yahoo.fr>
2024-04-19 15:32:57 +0200

    fix connexion bdd

--- filtres.php
+++ filtres.php
@@ -1,5 +1,5 @@
 <?php
-    require "bd.php";
+    require_once "bd.php";
     $bdd = getBD();
 
     if ($_POST["id"] == "Bgenre"){
--- rechercher.php
+++ rechercher.php
@@ -1,5 +1,5 @@
 <?php
-    require "bd.php";
+    require_once "bd.php";
     $bdd = getBD();
 
     if(isset($_POST["recherche"]) && $_POST["recherche"] != ""){
