b2ea0285a87f96c15da861610011d873e5757412
Cedrip <cedrichen15@gmail.com>
2024-04-20 18:02:22 +0200

    api wikipedia

--- connexion_base_donnee.php
+++ connexion_base_donnee.php
@@ -1,10 +1,6 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "prix_nobel";
-$mysqli = new mysqli($host, $user, $password, $database);
-if ($mysqli->connect_error) {
-    die("La connexion à la base de données a échoué : " . $mysqli->connect_error);
-}
+	function getBD(){
+		$bdd = new PDO('mysql:host=sql211.infinityfree.com;dbname=if0_36368472_prix_nobel;charset=utf8','if0_36368472', 'EamGfcaHBI0k');
+		return $bdd;
+	}
 ?>
